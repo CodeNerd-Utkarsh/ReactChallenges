84b058de925262e8733509facc97a0fbcb362cf5
CodeNerd-Utkarsh <hbase_groups_official@aol.com>
2023-02-17 14:26:43 +0530

    first challenge

---
+++ react_challenge_one/src/App.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import './App.css'
+
+type PointType = {
+  x: number,
+  y: number
+}
+function NewPoint(props: any) {
+  const { x, y } = props
+  return (
+    <div className='newPoint' style={{ top: y + "px", left: x + "px" }} >
+
+    </div>
+  )
+}
+
+function App() {
+  const [points, setPoints] = useState<PointType[]>([])
+  const [popped, setPopped] = useState<PointType[]>([])
+  function handleClicks(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    const { clientX, clientY } = e;
+    setPoints((prev) => [...prev, {
+      x: clientX,
+      y: clientY
+    }])
+  }
+  function clearPoints() {
+    setPoints([])
+  }
+  function handleundo() {
+    const newPoints = [...points]
+    const lastPoint = newPoints.pop()
+    setPopped([...popped, lastPoint])
+    setPoints(newPoints)
+    console.log(popped)
+  }
+
+  return (
+    <>
+      <button onClick={handleundo}>Undo</button>
+      <button onClick={clearPoints} >Reset</button>
+      <div className="app" onClick={(e) => handleClicks(e)} >
+        {points.map((point, idx) => {
+          return (
+            <NewPoint key={idx} {...point} />
+          )
+        })}
+      </div>
+    </>
+  )
+}
+
+export default App
